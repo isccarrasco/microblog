3d6177a1259ab01bc5351f677b6fcc9d69389b71
isccarrasco <mario.carrasco@gmail.com>
2017-05-08 11:10:26 -0500

    Creating the base of the project.
    Creating a simple Hello World Example, using the Flask micro framework.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import os
+import subprocess
+import sys
+
+subprocess.call(['virtualenv', 'flask'])
+if sys.platform == 'win32':
+  bin = 'Scripts'
+else:
+  bin = 'bin'
+
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', '--upgrade', 'flask'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', '--upgrade', 'flask-login'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', '--upgrade', 'flask-openid'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', '--upgrade', 'flask-mail'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', '--upgrade', 'flask-sqlalchemy'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', '--upgrade', 'sqlalchemy-migrate'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', '--upgrade', 'flask-whooshAlchemy'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', '--upgrade', 'flask-wtf'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', '--upgrade', 'flask-babel'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', '--upgrade', 'guess_language'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', '--upgrade', 'flipflop'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', '--upgrade', 'coverage'])
