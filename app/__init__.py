18750f72e64b2f33799ca2c0ad73c89160ef1a9f
isccarrasco <mario.carrasco@gmail.com>
2017-05-09 23:16:45 -0500

    Part V: User Logins

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
+from flask_login import LoginManager
+from flask_openid import OpenID
+
+from config import basedir
 
 # Instance of Flask
 app = Flask(__name__)
@@ -8,5 +15,12 @@ app.config.from_object('config')
 # Instance of SQLAlchemy and binding with Flask application
 db = SQLAlchemy(app)
 
-from app import views, models
+# Instance of LoginManager and binding with Flask app
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
 
+# Instance of OpenID
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+from app import views, models
