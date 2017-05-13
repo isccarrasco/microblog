ef3a208879b7ae770677eb7656b0de4389303808
isccarrasco <mario.carrasco@gmail.com>
2017-05-13 12:40:52 -0500

    Part VIII: Followers, Contacts And Friends
    Adding functionality for create the self-referential relationship to
    mange the followers, implement the migrate and testings for this case

--- app/models.py
+++ app/models.py
@@ -3,6 +3,12 @@ from flask_login import UserMixin
 from app import db
 
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     social_id = db.Column(db.String(64), nullable=False, unique=True, default='')
@@ -11,6 +17,13 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    followed = db.relationship('User',
+                               secondary=followers,
+                               primaryjoin=(followers.c.follower_id == id),
+                               secondaryjoin=(followers.c.followed_id == id),
+                               backref=db.backref('followers', lazy='dynamic'),
+                               lazy='dynamic')
 
     @property
     def is_authenticated(self):
@@ -23,16 +36,6 @@ class User(UserMixin, db.Model):
     @property
     def is_anonymous(self):
         return False
-
-    def get_id(self):
-        try:
-            return unicode(self.id)
-        except:
-            return str(self.id)
-
-    def avatar(self, size):
-        gravatar_email = self.email if self.email is not None else 'example@mail.com'
-        return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(gravatar_email.encode('utf-8')).hexdigest(), size)
 
     @staticmethod
     def make_unique_nickname(nickname):
@@ -48,7 +51,33 @@ class User(UserMixin, db.Model):
             version += 1
 
         return new_nickname
-    
+
+    def get_id(self):
+        try:
+            return unicode(self.id)
+        except:
+            return str(self.id)
+
+    def avatar(self, size):
+        gravatar_email = self.email if self.email is not None else 'example@mail.com'
+        return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(gravatar_email.encode('utf-8')).hexdigest(), size)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+            return self
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+            return self
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        return Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id).order_by(Post.timestamp.desc())
+
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
@@ -61,3 +90,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
+
+
+
+
